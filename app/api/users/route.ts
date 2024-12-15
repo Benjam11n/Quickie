fcaebace2c2fe534b43e84979b5a8ac51fe3c8f6
Benjamin Wang <ben.wang9000@gmail.com>
2024-12-15 21:16:01 +0800

    Add error handling

---
+++ app/api/users/route.ts
@@ -0,0 +1,14 @@
+import dbConnect from '@/lib/mongoose';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const users = await User.find();
+
+    return NextResponse.json({ success: true, data: users }, { status: 200 });
+  } catch (error) {
+    return handleError(error, 'api') as ApiErrorResponse;
+  }
+}
