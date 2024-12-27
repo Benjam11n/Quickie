9dc47b79650bfbf0dab2cffca91eefd9a3334dd9
Benjamin Wang <ben.wang9000@gmail.com>
2024-12-27 18:18:07 +0800

    set up middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,37 @@
-export { auth as middleware } from '@/auth';
+import { NextResponse, NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+import { ROUTES } from './constants/routes';
+
+export default async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
+  const isAuthenticated = !!token;
+
+  const pathname = req.nextUrl.pathname;
+
+  const protectedRoutes = ['/profile', '/wishlists', '/collection', '/boards'];
+
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  if (isProtectedRoute) {
+    if (!isAuthenticated) {
+      const url = new URL(ROUTES.SIGN_IN, req.url);
+      url.searchParams.set('callbackUrl', pathname);
+      return NextResponse.redirect(url);
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/profile',
+    '/wishlists/:path*',
+    '/collection/:path*',
+    '/boards/:path/edit',
+    '/boards/new',
+  ],
+};
